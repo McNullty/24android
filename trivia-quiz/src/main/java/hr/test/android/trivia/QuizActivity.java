931f47a55b6628653b4cadf0989bd0d1ebc52674
Mladen Čikara <mladen.cikara@gmail.com>
2013-04-25 21:37:43 +0200

    Sređen help ekran

--- trivia-quiz/src/main/java/hr/test/android/trivia/QuizActivity.java
+++ trivia-quiz/src/main/java/hr/test/android/trivia/QuizActivity.java
@@ -4,4 +4,6 @@ import android.app.Activity;
 
 public class QuizActivity extends Activity {
 	public static final String GAME_PREFERENCES = "GamePrefs";
+
+	public static final String DEBUG_TAG = "Trivia Quiz Log";
 }
